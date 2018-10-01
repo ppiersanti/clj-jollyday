ed9c83c41d5a7513fba98db94963d9b6268b4969
Paolo Piersanti <yaathey@gmail.com>
2018-10-01 23:26:08 +0200

    wip: customize a configuration

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [de.jollyday/jollyday "0.5.2"]])
+                 [de.jollyday/jollyday "0.5.2"]
+                 [org.clojure/data.xml "0.2.0-alpha5"]])
--- src/clj_jollyday.clj
+++ src/clj_jollyday.clj
@@ -1,10 +1,13 @@
 (ns clj-jollyday
-  (:require [clojure.string :as string]
-            [clj-jollyday.util :as util]
-            [clojure.string :as str]
+  (:require [clj-jollyday.util :as util]
+            [clojure
+             [string :as string]
+             [xml :as cx]
+             [zip :as cz]]
+            [clojure.data.xml :as cdx]
             [clojure.java.io :as io])
-  (:import [de.jollyday HolidayManager HolidayCalendar]
-           [java.time LocalDate]))
+  (:import [de.jollyday HolidayCalendar HolidayManager]
+           java.time.LocalDate))
 
 
 (def predefined-calendars
@@ -13,28 +16,48 @@
         (fn [^String n fmt]
           [(string/lower-case (.replace n \_ \-)) fmt]))))
 
+(defprotocol CreateManager
+  (create-manager [input]))
+
+(extend-protocol CreateManager
+  String
+  (create-manager [xml]
+    (let [is            (java.io.ByteArrayInputStream. (.getBytes xml))
+          _             (de.jollyday.HolidayManager/setManagerCachingEnabled false)
+          util          (de.jollyday.util.XMLUtil.)
+          configuration (.unmarshallConfiguration util is)
+          cds           (proxy [de.jollyday.datasource.ConfigurationDataSource] []
+                          (getConfiguration [param]
+                            configuration))
+          man           (doto (HolidayManager/getInstance)
+                          (.setConfigurationDataSource cds)
+                          (.doInit))]
+      man))
+
+  clojure.lang.Keyword
+  (create-manager [calendar]
+    (let [calendar' (cond
+                      (keyword? calendar)                  (get
+                                                            predefined-calendars
+                                                            (name calendar))
+                      (instance? HolidayCalendar calendar) calendar)]
+      (when-not (instance? HolidayCalendar calendar')
+        (throw (IllegalArgumentException. (str  calendar " was not a valid calendar"))))
+      (HolidayManager/getInstance calendar'))))
+
 (defn manager
   "Given a country calendar (e.g. :italy) as an argument returns an HolidayManager
   that can be utilized to compute holidays for that country."
-  ([]
-   (HolidayManager/getInstance))
-  ([calendar]
-   (let [calendar' (cond
-                     (keyword? calendar)                  (get
-                                                           predefined-calendars
-                                                           (name calendar))
-                     (instance? HolidayCalendar calendar) calendar)]
-     (when-not (instance? HolidayCalendar calendar')
-       (throw (IllegalArgumentException. (str  calendar " was not a valid calendar"))))
 
-     (HolidayManager/getInstance calendar'))))
+  [input]
+  (create-manager input))
 
 (defn holiday->map
   ""
   [h]
   (hash-map
    :date            (.getDate h)
-   :properties-key  (keyword (str/lower-case (.getPropertiesKey h)))
+   :properties-key  (keyword (string/lower-case (.getPropertiesKey h)))
    :description     (.getDescription h)))
 
 (defn holidays
@@ -61,6 +84,27 @@
   (.isHoliday hm d (into-array [""])))
 
 
+
+
+
+(comment
+  ;; poc to create an holiday manager from a customized configuration stream
+  (let [xml           (slurp (io/resource "test.xml"))
+        is            (java.io.ByteArrayInputStream. (.getBytes xml))
+        xml           (cz/xml-zip (cx/parse is))
+        hloc          (-> xml (cz/down))
+        holidays_node (-> hloc
+                          (cz/append-child {:tag     :tns:FixedWeekday
+                                            :attrs   {:weekday                  "SUNDAY"
+                                                      :which                    "FIRST"
+                                                      :month                    "NOVEMBER"
+                                                      :descriptionPropertiesKey "MY_CUSTOM"}
+                                            :content nil}))
+
+        m (manager (cdx/emit-str
+                    (cz/root holidays_node)))]
+
+    (holiday? m (java.time.LocalDate/of 2018 11 4))))
 
 (comment
   ;; poc to initialize an holiday manager from a holiday configuration stream
