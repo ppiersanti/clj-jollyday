18e2194fab2a3b4bfd6030a5ecaf91f5f0f9ab99
Paolo Piersanti <slowcod@gmail.com>
2019-08-18 22:58:55 +0200

    chore: move test resources to test resourcews directory

--- project.clj
+++ project.clj
@@ -6,7 +6,9 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [de.jollyday/jollyday "0.5.2"]
                  [org.clojure/data.xml "0.2.0-alpha5"]
-                 [org.clojure/data.zip "0.1.2"]])
+                 [org.clojure/data.zip "0.1.2"]]
+  :profiles {:test {:resource-paths ["test-resources"]}})
+
 
 (cemerick.pomegranate.aether/register-wagon-factory!
  "http" #(org.apache.maven.wagon.providers.http.HttpWagon.))
